24d53bc49a3e4dd566aed479a5a75f5814952c7c
Naresh2023 <113895382+Naresh2023@users.noreply.github.com>
2022-09-19 02:30:17 -0700

    Add files via upload

---
+++ fireconfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCfvOx7SKIdRYdQazBJVq_2to1jpZYw3qw",
+    authDomain: "projectdb-4cd7f.firebaseapp.com",
+    databaseURL: "https://projectdb-4cd7f-default-rtdb.firebaseio.com",
+    projectId: "projectdb-4cd7f",
+    storageBucket: "projectdb-4cd7f.appspot.com",
+    messagingSenderId: "1045487088114",
+    appId: "1:1045487088114:web:642065fd719f440cdfbb6b",
+    measurementId: "G-X5BEWXWB24"
+  };
+  firebase.initializeApp(firebaseConfig)
